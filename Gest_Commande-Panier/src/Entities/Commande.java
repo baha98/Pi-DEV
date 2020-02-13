527557fa7a6ac85cf97a5f35fe6987124d831c11
SaidiAhmed11 <49171816+SaidiAhmed11@users.noreply.github.com>
2020-02-13 19:56:13 +0000

    Gest_Commande-Panier

---
+++ Gest_Commande-Panier/src/Entities/Commande.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+
+import java.sql.Date;
+import java.sql.*;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Commande {
+    private int Id_Commande;
+    private Date Date_Commande;
+    private Double Prix_Total;
+    private int Id_Membre;
+
+    
+    
+
+    public Commande() {
+        
+    }
+    
+    public Commande(int Id_Commande,Date Date_Commande, Double Prix_Total , int Id_Membre) {
+        this.Id_Commande=Id_Commande;
+        this.Date_Commande = Date_Commande;
+        this.Prix_Total = Prix_Total;
+        this.Id_Membre = Id_Membre;
+    }
+
+    public Commande(int Id_Commande) {
+        this.Id_Commande = Id_Commande;
+    }
+
+    public Commande(int Id_Commande,Date Date_Commande ,Double Prix_Total) {
+        this.Id_Commande = Id_Commande;
+        this.Date_Commande = Date_Commande;
+        this.Prix_Total = Prix_Total;
+    }
+    
+    public Commande(Date Date_Commande, Double Prix_Total , int Id_Membre) {
+        
+        this.Date_Commande = Date_Commande;
+        this.Prix_Total = Prix_Total;
+        this.Id_Membre = Id_Membre;
+    }
+
+    public int getId_Commande() {
+        return Id_Commande;
+    }
+
+    public Date getDate_Commande() {
+        return Date_Commande;
+    }
+
+    public Double getPrix_Total() {
+        return Prix_Total;
+    }
+
+    public void setId_Commande(int Id_Commande) {
+        this.Id_Commande = Id_Commande;
+    }
+
+    public void setDate_Commande(Date Date_Commande) {
+        this.Date_Commande = Date_Commande;
+    }
+
+    @Override
+    public String toString() {
+        return "Commande{" + "Id_Commande=" + Id_Commande + ", Date_Commande=" + Date_Commande + ", Prix_Total=" + Prix_Total + ", Id_Membre=" + Id_Membre + '}';
+    }
+
+    public void setPrix_Total(Double Prix_Total) {
+        this.Prix_Total = Prix_Total;
+    }
+    
+    public void setId_Membre(int Id_Membre) {
+        this.Id_Membre = Id_Membre;
+    }
+
+    public int getId_Membre() {
+        return Id_Membre;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + this.Id_Commande;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Commande other = (Commande) obj;
+        if (this.Id_Commande != other.Id_Commande) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
